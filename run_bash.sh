4ac620ac0277e4b343c606ce7b7d2fd454dbfd07
Hellod035 <hellod035@163.com>
2025-03-05 18:03:47 +0800

    remove motion_lib

--- run_bash.sh
+++ run_bash.sh
@@ -1,17 +1,8 @@
 ### Before run any program, press L2 + R2 to get into debug mode !!! ### 
 
 # RUN POLICY
-# Press select button to quit the program at any time.
-python <deploy.py | deploy_motion_lib.py> <network interface> <xxx.yaml>
+python deploy.py <network interface> <xxx.yaml>
 
-# 1. Start button to move to default pose.
-# 2. A button to eval the policy.
-
-
-# G1 flat
-python deploy.py <network interface> g1.yaml
-
-
-# G1 Height Map
-# Press B button to use the GT Height Map
-python deploy.py <network interface> g1_height_map.yaml
+# 1. Press Start button to move to default pose.
+# 2. Press A button to eval the policy.
+# 3. Press Select button to quit the program at any time.
